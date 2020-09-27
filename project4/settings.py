a09341eecea7515556c84f9fe8aa7433ce2bb5b1
khongchai <world1955@hotmail.com>
2020-09-27 23:53:52 +0700

    final push II

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =     ["*"]
+ALLOWED_HOSTS =     []
 
 
 # Application definition
@@ -84,6 +84,18 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 AUTH_USER_MODEL = "network.User"
 
 # Password validation
